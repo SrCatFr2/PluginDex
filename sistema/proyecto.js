1442fe6c6213468476093234dc52e83b9d48021d
SrCatFr2 <sr.cat.accs@gmail.com>
2026-09-17 15:44:08 -0700

    Create proyecto.js

---
+++ sistema/proyecto.js
@@ -0,0 +1,29 @@
+window.PluginDex = {
+
+    version: "1.0.0",
+
+    project: {
+
+        name: "Mi proyecto",
+
+        page: {
+
+            background: "#eeeeee",
+
+            glass: false,
+
+            elements: []
+
+        },
+
+        settings: {
+
+            width: 850,
+
+            minHeight: 600
+
+        }
+
+    }
+
+};
